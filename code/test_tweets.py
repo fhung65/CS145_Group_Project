fc8f35d0ae2cbe6d42dbabb081ad32d5d7981d3a
Fan Hung <fhung65@gmail.com>
2017-12-09 20:50:39 -0800

    more testing with doc2vec

---
+++ code/test_tweets.py
@@ -0,0 +1,20 @@
+test_tweets = [
+    ['whats', 'up'],
+    ['hello'],
+    ['hey'],
+    ['hi'],
+    ['yo'],
+    ['whatchu', 'doing'],
+    ['what', 'are', 'you', 'doing'],
+    ['how', 'are', 'you', 'doing'],
+    ['what', 'are', 'you', 'up', 'to'],
+    ['I', 'like', 'cookies'],
+    ['how', 'does', 'that', 'work'],
+    ['sandwich'],
+    ['sadwiches'],
+    ['I', 'like', 'sandwiches'],
+    ['I', 'like', 'sandwich'],
+    ['I', 'like', 'sandwitch'],
+    ['I', 'like', 'burgers'],
+]
+
