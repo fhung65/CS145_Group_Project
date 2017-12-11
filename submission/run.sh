0a6fbe73d027ddae956ebac7b2e7ba08483026b7
Jai Srivastav <jaiakt@gmail.com>
2017-12-10 23:39:48 -0800

    recent changes

--- submission/run.sh
+++ submission/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-python3 code/scraper.py
+cd codes
+#python3 code/scraper.py tweets4.csv tweets4_preprocessed.csv
+#python3 code/test_doc2vec.py
+python3 neural.py tweets4_preprocessed.csv doc2vec_model.model
